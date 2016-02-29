30e62c3517f8437b6c017166719f9c99d13dca08
Josh Bivens <hotbioform@gmail.com>
2016-02-28 22:33:22 -0700

    Add utils object to constructor example

---
+++ utils.js
@@ -0,0 +1,46 @@
+function Item(name, number) {
+  this.name = name;
+  this.number = number;
+}
+
+var foo = new Item("foo", 42);
+var bar = new Item("bar", 53);
+var baz = new Item("baz", 22);
+
+function Inventory() {
+  this.items = [];
+  this.add = function(item) {
+    this.items.push(item);
+  };
+}
+
+var myInv = new Inventory();
+myInv.add(foo);
+myInv.add(bar);
+myInv.add(baz);
+
+var utils = {
+  print: function (items) {
+    items.map(x => console.log("Item name: " + x.name + "\nItem No.: " + x.number + "\n"));
+  },
+  sort: function(items, key) {
+    if(key == "number") {
+      items.sort((a, b) => a.number - b.number);
+    } else if (key == "name") {
+      items.sort((a, b) => {
+        if(a.name < b.name) return -1;
+        if(a.name > b.name) return 1;
+        0;
+      });
+    }
+    return this.print(items);
+  },
+  total: function(items) {
+    var total = 0;
+    items.map(x => total += x.number);
+    return total;
+  }
+};
+
+utils.sort(myInv.items, "name");
+utils.total(myInv.items);
